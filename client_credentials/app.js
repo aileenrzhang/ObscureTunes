58d1f6b883fd5e297b356d1e60c3ec699253349c
Aileen Zhang <61919893+aileenrzhang@users.noreply.github.com>
2022-11-22 01:06:08 -0800

    Add documentation to App.js

--- client_credentials/app.js
+++ client_credentials/app.js
@@ -1,10 +1,6 @@
 /**
- * This is an example of a basic node.js script that performs
- * the Client Credentials oAuth2 flow to authenticate against
- * the Spotify Accounts.
- *
- * For more information, read
- * https://developer.spotify.com/web-api/authorization-guide/#client_credentials_flow
+ * This script performs the Client Credential oAuth2 flow to
+ * authenticate Spotify accounts.
  */
 
 var request = require('request'); // "Request" library
